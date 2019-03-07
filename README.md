776a5690b020e6d01b296d4bfc0062bebb2256c0
Timothy <31439772+SyncdeveloperT@users.noreply.github.com>
2019-03-07 15:42:44 +0100

    crap deleted

--- README.md
+++
@@ -1,2 +0,0 @@
-# ProjectSpeler
-Project opdracht 
--- phptoets/Aanpassen.php
+++
@@ -1,7 +0,0 @@
-<?php
-$sql = "SELECT * FROM schoolbezoek WHERE id=".$_POST['id'];
- echo 'Tot hier en niet verder';
- echo ("<br>");
- echo ("<br>");
-echo " <tr> <td><a href='Form.php'>Hier log je uit</a></td>";
-?>
