750449286eafae935657eb98671ac46c1c948f9f
Demian de Jager <31468669+DemianSync@users.noreply.github.com>
2019-03-11 16:13:00 +0100

    cookies AVG

--- policy.php
+++ policy.php
@@ -18,7 +18,7 @@
 <meta name="description" content="Neem voor vragen, opmerkingen of suggesties contact met ons op!">      
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Contact</title>
+<title>Cookie beleid |</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-
 BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
